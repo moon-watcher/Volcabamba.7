85db823ecfa8e0a3a94e5ea229293e8c70cb0d34
langares <langares@yahoo.es>
2022-06-02 01:22:12 +0200

    Add Player0 entity

---
+++ src/entities/Player0/components.h
@@ -0,0 +1,23 @@
+// #pragma once
+
+// #include "inc/components.h"
+
+
+// #define COMPONENTS(entity)                          \
+//     Components         *comps = entity->components; \
+//     ComponentRigidbody *rb    = &comps->rigidbody;  \
+//     ComponentSprite    *sp    = &comps->sprite;     \
+//     ComponentPosition  *cp    = &rb->position;      \
+//     ComponentVelocity  *cv    = &rb->velocity;      \
+//     ComponentInput     *ci    = &comps->input;      \
+//     Joyreader          *joy   = &ci->joy;
+
+
+// typedef struct
+// {
+//     ComponentRigidbody rigidbody;
+//     ComponentSprite    sprite;
+//     ComponentInput     input;
+//     ComponentAttribute attrs;
+// }
+// Components;
